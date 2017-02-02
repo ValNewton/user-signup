ac23912ab85c4196f812a54e7e3e2df123c613fa
Valerie Newton <valcnewton@gmail.com>
2017-02-02 14:39:27 -0600

    New copy - last update was wrong file

--- main.py
+++ main.py
@@ -14,54 +14,117 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import webapp2
 import cgi
+import re
+from string import letters
 
-def build_page(content):
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FlickList</title>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
+def build_page():
     
     u_name_label = "<label>Username</label>"
-    u_name = "<input type="text" name="username"/>"
+    u_name_label = "<input type= "text" name= "username" />"
 
     p_label = "<label>Password</label>"
-    password = "<input type = "password" name = "password"/>"
+    pass_ = "<input type = "password" name = "password"  />"
 
-    p_ver_label = "<label>Verify Password</label>"
-    p_verify = "<input type = "password" name = "verify"/>"
+    ver_label = "<label>Verify Password</label>"
+    p_verify = "<input type = "password" name = "verify" />"
 
     email_label = "<label>Email(optional)</label>"
-    email = "<input type ="email" name ="email"/>"
+    email = "<input type = "email" name = "email" />"
 
     header = "<h2>Signup</h2>"
     submit = "<input type ='submit'/>"
 
-    form = "<form method='post'>" +
-            u_name_label + u_name + <br> +
-            p_label + password + <br> +
-            p_ver_label + p_verify + <br> +
+    form = "<form action= "/welcome" method="post">" + \
+            u_name_label + u_name + "<span class="error"></span>" + "<br>" + \
+            p_label + pass_ +"<br>" + "<span class="error"></span>" + "<br>" + \
+            ver_label + p_verify + "<span class="error"></span>" + "<br>" + \
             submit + "</form>"
 
-    return header + format
+    return header + form
 
-class MainHandler(webapp2.RequestHandler):
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Index(webapp2.RequestHandler):
     def get(self):
-        content = build_page("")
+        page_content = build_page()
+        content = page_header + page_content + page_footer
         self.response.write(content)
 
     def post(self):
+        have_error = False
         username = self.request.get("username")
         password = self.request.get("password")
         verify = self.request.get("verify")
         email = self.request.get("email")
 
+        params = dict(username=username,
+                      email=email)
+
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+
 class Welcome(webapp2.RequestHandler):
     def get(self):
         username = self.request.get('username')
-        greeting = "Welcome" + username
-        content = page_header + "<p>" + greeting + "</p>" + page_footer
+        greeting = "Welcome, " + username
+        content = "<p>" + greeting + "</p>"
         self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
+    ('/', Index),
     ('/welcome', Welcome),
     ], debug=True)
